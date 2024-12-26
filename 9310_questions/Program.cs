09eed506cc38f36013b0f1bcd94d984c1576da80
Gideon Yuval <gideonyuval@gmail.com>
2024-12-26 08:53:34 +0200

    Add project files.

---
+++ 9310_questions/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9310_questions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> q;
+
+            //1
+            q = Arr2Q(new int[] { 0, 9, 1, 7, 3, 9, 1, 3, 2, 1 });
+            Console.WriteLine(Max2Digit(q));
+            q = Arr2Q(new int[] { 0, 0, 1, 4, 3, 3, 1, 3, 2, 8 });
+            Console.WriteLine(Max2Digit(q));
+
+            //2
+            q = Arr2Q(new int[] { 17, 1, 17, 2, 7, 6, 36, 2, 1, 2, 5, 5 });
+            RemoveDups(q);
+            Console.WriteLine(q);
+
+            //3
+            q = Arr2Q(new int[] { 2, 4, 1, 5, 2, 8, 9, 1, 5, 7 });
+            Console.WriteLine(QCouples(q));
+
+            //4
+            q = Arr2Q(new int[] { 2, 3, 5, 8, 13, 4, 4, 8, 3, 6, 9, 15 });
+            Console.WriteLine(FibSeq(q));
+
+            //5
+            q = Arr2Q(new int[] { 20, 5, 2, 1, 2, 36, 6, 3, 2, 17, 1, 17 });
+            Console.WriteLine(ProductsQ(q));
+            q = Arr2Q(new int[] { 20, 5, 2, 3, 17, 1, 17 });
+            Console.WriteLine(ProductsQ(q));
+            q = Arr2Q(new int[] { 20, 5, 2, 3 });
+            Console.WriteLine(ProductsQ(q));
+
+
+        }
+
+        static Queue<int> Arr2Q(int[] arr)
+        {
+            Queue<int> q = new Queue<int>();
+            foreach (int value in arr)
+                q.Insert(value);
+            return q;
+        }
+
+        //1
+        //input: queue of digits (0-9)
+        //output: max 2 digit number which can be composed of the digits
+        //assume: at least 2 digits
+        //example: 0, 9, 1, 7, 3, 9, 1, 3, 2, 1   return 99
+        //example: 0, 0, 1, 4, 3, 3, 1, 3, 2, 8   return 84
+        //cannot use array, don't need to restore Q
+        //what is the complexity?
+
+        static int Max2Digit(Queue<int> q)
+        {
+            return 0;
+        }
+
+
+        //2
+        //input: queue of ints
+        //output: void, queue is without duplicates
+        //example: queuehead = [17,1,17,2,7,6,36,2,1,2,5,5] -> queuehead = [17,1,2,7,6,36,5] 
+        //cannot use array
+        //what is the complexity?
+
+        static void RemoveDups(Queue<int> q) //O(n^2)
+        {
+
+        }
+
+        //3
+        //input: queue of ints
+        //output: new queue, with only the ints which appear EXACTLY twice
+        //example: queuehead = [2,4,1,5,2,8,9,1,5,7], new queuehead = [2,1,5] 
+        //notes: no need to restore original q
+        //what is the complexity?
+
+        static Queue<int> QCouples(Queue<int> q)
+        {
+            return null;
+        }
+
+        //4
+        //input: queue of ints, made from Fibonacci sequences (no gaps). Minimum queue length = 2
+        //output: new queue, with Fibonacci sequence lengths
+        //example: queuehead = [2,3,5,8,13,4,4,8,3,6,9,15], new queuehead = [5,3,4] 
+        //notes: no need to restore original q
+        //tip: what's the minimum sequence length?
+        //what is the complexity?
+        static Queue<int> FibSeq(Queue<int> q)
+        {
+            return null;
+        }
+
+
+        //5
+        //input: queue of ints, minimum length = 2
+        //output: True, if the first number in a sequence = multiplication of the following numbers
+        //example: queuehead = [20,5,2,1,2,36,6,3,2,17,1,17], return: true
+        //example: queuehead = [20,5,2,3,17,1,17], return: false
+        //example: queuehead = [20,5,2,3], return: false
+        //notes: no need to restore original q
+        //שימו לב, מכפלה באנגלית זה product
+        //what is the complexity?
+
+        static bool ProductsQ(Queue<int> q)
+        {
+            return false;
+        }
+
+
+    }
+}
---
+++ 9310_questions/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("9310_questions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("9310_questions")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("97aae509-d26f-4711-8de1-b01bbfc55561")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
